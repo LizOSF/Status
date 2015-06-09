aeaaddfa1abe015fbef3cdc8f7433d11cb7d2b32
LizOSF <lizodonnell@Elisabeths-MacBook-Pro.local>
2015-06-08 19:28:27 -0700

    Add table view with data source

--- Status/ViewController.swift
+++ Status/ViewController.swift
@@ -7,24 +7,36 @@
 //
 
 // MVC
-// M  Data. classes that aren't any subclasses. like a user, or status. The model is the backbone of your app. Every app has a model layer. Typically, start with user model. A user model is a code representaiton of a user. 
+// M  Data. classes that aren't any subclasses. like a user, or status. The model is the backbone of your app. Every app has a model layer. Typically, start with user model. A user model is a code representaiton of a user.
 // V
 // C . View controller.
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO: Return count of update items
+        return 0
     }
-
+    
+ //MARK: - UITableViewDataSource
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // TODO: Make this cell reuseable
+        var cell = UITableViewCell()
+        return cell
+        
+    }
+    
+   
 
 }
 
