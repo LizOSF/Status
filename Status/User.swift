ff0ce70f091f1c171813e00e81d9d12a2ed1d168
LizOSF <lizodonnell@Elisabeths-MacBook-Pro.local>
2015-06-08 19:12:27 -0700

    Add models

---
+++ Status/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Status
+//
+//  Created by Elisabeth O'Donnell on 6/8/15.
+//  Copyright (c) 2015 Elisabeth O'Donnell. All rights reserved.
+
+//
+
+//uppercase for classes. lowercase for almost anything else.
+
+import Foundation
+
+class User {
+    var name: String?
+    var bio: String?
+    var username: String?
+    var link: String?
+    var city: String?
+}
--- Status/ViewController.swift
+++ Status/ViewController.swift
@@ -6,6 +6,11 @@
 //  Copyright (c) 2015 Elisabeth O'Donnell. All rights reserved.
 //
 
+// MVC
+// M  Data. classes that aren't any subclasses. like a user, or status. The model is the backbone of your app. Every app has a model layer. Typically, start with user model. A user model is a code representaiton of a user. 
+// V
+// C . View controller.
+
 import UIKit
 
 class ViewController: UIViewController {
